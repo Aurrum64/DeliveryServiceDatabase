79207fa4625f1d55086cfca2156994bf92c7aed6
Artem Lebedev <aurrum64@gmail.com>
2019-08-19 09:21:29 +0300

    Почистил лишнее от merge

--- src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/Application.java
+++ src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/Application.java
@@ -3,8 +3,6 @@ package ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// комментарий для того, чтобы перенести проект в ветку Develop
-
 @SpringBootApplication
 public class Application {
 

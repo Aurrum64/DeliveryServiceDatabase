6a8fd5dd07d271f968bb2a6304f77027a4d49ca9
Artem Lebedev <aurrum64@gmail.com>
2019-08-20 11:58:21 +0300

    Протестировал построение маршрутов через несколько точек

--- src/main/resources/static/js/graphHopperRoutes.js
+++ src/main/resources/static/js/graphHopperRoutes.js
@@ -1,7 +1,19 @@
 L.Routing.control({
     waypoints: [
-        L.latLng(55.79163, 37.63453),
-        L.latLng(55.84988, 37.56672)
+        L.latLng(55.687086, 37.529789),
+        L.latLng(55.782392, 37.614924),
+        L.latLng(55.694843, 37.435023)
+    ],
+    router: L.Routing.graphHopper('585def41-7ae7-4420-a2b3-b71c919bc166'),
+    routeWhileDragging: true
+}).addTo(myDeliveryServiceMap);
+
+L.Routing.control({
+    waypoints: [
+        L.latLng(55.642063, 37.656123),
+        L.latLng(55.684758, 37.738521),
+        L.latLng(55.833436, 37.715175),
+        L.latLng(55.790139, 37.814052)
     ],
     router: L.Routing.graphHopper('585def41-7ae7-4420-a2b3-b71c919bc166'),
     routeWhileDragging: true
--- src/main/resources/static/js/markers.js
+++ src/main/resources/static/js/markers.js
@@ -1,3 +1,20 @@
-var marker = L.marker([55.7422, 37.5719]).addTo(myDeliveryServiceMap);
+var marker1 = L.marker([55.7422, 37.5719]).addTo(myDeliveryServiceMap);
+var marker2 = L.marker([55.684758, 37.738521]).addTo(myDeliveryServiceMap);
+var marker3 = L.marker([55.833436, 37.715175]).addTo(myDeliveryServiceMap);
+var marker4 = L.marker([55.687086, 37.529789]).addTo(myDeliveryServiceMap);
+var marker5 = L.marker([55.782392, 37.614924]).addTo(myDeliveryServiceMap);
+var marker6 = L.marker([55.642063, 37.656123]).addTo(myDeliveryServiceMap);
+var marker7 = L.marker([55.826479, 37.487208]).addTo(myDeliveryServiceMap);
+var marker8 = L.marker([55.694843, 37.435023]).addTo(myDeliveryServiceMap);
+var marker9 = L.marker([55.790139, 37.814052]).addTo(myDeliveryServiceMap);
 
-marker.bindPopup("Курьер №1").openPopup();
+
+marker1.bindPopup("Курьер №1");
+marker2.bindPopup("Курьер №2");
+marker3.bindPopup("Курьер №3");
+marker4.bindPopup("Курьер №4");
+marker5.bindPopup("Курьер №5");
+marker6.bindPopup("Курьер №6");
+marker7.bindPopup("Курьер №7");
+marker8.bindPopup("Курьер №8");
+marker9.bindPopup("Курьер №9");

e2d63797f7666c7b6f70e1bc1719cd5ac62f19dc
polinagamina <polina.gamina@yandex.ru>
2019-08-26 00:23:27 +0300

    Добавила страничку заказов

--- src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/controllers/OrdersController.java
+++ src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/controllers/OrdersController.java
@@ -3,10 +3,15 @@ package ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.entities.OrderDetailsEntity;
 import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.entities.Orders;
 import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.repositories.OrdersRepository;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class OrdersController {
@@ -17,10 +22,14 @@ public class OrdersController {
     public OrdersController(OrdersRepository ordersRepository) {
         this.ordersRepository = ordersRepository;
     }
-
-    @GetMapping("/addOrder")
-    public @ResponseBody
-    String addNewOrder(@RequestParam Integer departmentId,
+    @GetMapping("/orders")
+    public String ordersView(Map<String, Object> model) {
+        Iterable<Orders> orders = ordersRepository.findAll();
+        model.put("orders", orders);
+        return "orders";
+    }
+    @PostMapping("/orders")
+    public String addNewOrder(@RequestParam Integer departmentId,
                        @RequestParam Integer managerId,
                        @RequestParam Integer courierId,
                        @RequestParam String paymentMethod,
@@ -34,6 +43,17 @@ public class OrdersController {
         orders.setOrderPrice(orderPrice);
         orders.setDiscount(discount);
         ordersRepository.save(orders);
-        return "Order saved.";
+        return "redirect:/orders";
+    }
+    @PostMapping("/ordersFilter")
+    public String findByOrderPayment(@RequestParam String paymentMeth, Map<String, Object> model) {
+        Iterable<Orders> orders;
+        if (paymentMeth != null && !paymentMeth.isEmpty()) {
+            orders = ordersRepository.findByPaymentMethod(paymentMeth);
+        } else {
+            orders = ordersRepository.findAll();
+        }
+        model.put("orders", orders);
+        return "orders";
     }
 }
--- src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/repositories/OrdersRepository.java
+++ src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/repositories/OrdersRepository.java
@@ -1,7 +1,11 @@
 package ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.entities.OrderDetailsEntity;
 import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.entities.Orders;
 
+import java.util.List;
+
 public interface OrdersRepository extends CrudRepository<Orders, Integer> {
+    List<Orders> findByPaymentMethod(String orderPrice);
 }

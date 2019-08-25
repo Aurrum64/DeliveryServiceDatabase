c1ed8664db2855e7523d37b996d810cbb3e68fcd
Artem Lebedev <Aurrum64@gmail.com>
2019-08-25 12:51:01 +0300

    Добавил возможность регистрации пользователей

---
+++ src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/controllers/RegistrationController.java
@@ -0,0 +1,38 @@
+package ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.entities.RolesEntity;
+import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.entities.UsersEntity;
+import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.repositories.UsersRepository;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(UsersEntity user, Map<String, Object> model) {
+        UsersEntity userFromDb = usersRepository.findByUsername(user.getUsername());
+        if (userFromDb != null){
+            model.put("message", "User exists!");
+            return "registration";
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(RolesEntity.USER));
+        usersRepository.save(user);
+        return "redirect:/login";
+    }
+
+}

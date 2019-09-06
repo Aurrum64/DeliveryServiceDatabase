1d6d6d08ad59a11d8d7ea1dba30d8ff54e8047c0
Artem Lebedev <Aurrum64@gmail.com>
2019-09-06 22:38:20 +0300

    Добавил функцию построения маршрутов по кнопке

--- src/main/resources/static/js/graphHopperRouting.js
+++ src/main/resources/static/js/graphHopperRouting.js
@@ -1,51 +1,30 @@
-jQuery(document).ready(function ($) {
+$(document).ready((function () {
     $("#buildRoute").click(function () {
-        console.log(couriersMarkersLayerGroup);
-        console.log(couriersMarkers[0]);
-        console.log(couriersMarkers[0]._latlng.lat);
-        console.log(couriersMarkers[0]._latlng.lng);
-        console.log(deliveryMarkers[0]);
-        console.log(deliveryMarkers[0]._latlng.lat);
-        console.log(deliveryMarkers[0]._latlng.lng);
-        L.Routing.control({
-            waypoints: [
-                L.latLng(55.7422, 37.5719),
-                L.latLng(55.687086, 37.529789)
-            ],
-            lineOptions: {
-                styles: [{color: 'green', opacity: 1, weight: 4}]
-            },
-            router: L.Routing.graphHopper('585def41-7ae7-4420-a2b3-b71c919bc166'),
-            routeWhileDragging: true
-        }).addTo(myDeliveryServiceMap);
+        if (couriersMarkers[0] === undefined && deliveryMarkers[0] === undefined) {
+            alert("Please, add courier with coordinates to the system first " +
+                "and add courier's markers to the map!\n" +
+                "Please, add order details with address to the system first " +
+                "and add delivery markers to the map!");
+        } else if (couriersMarkers[0] === undefined) {
+            alert("Please, add courier with coordinates to the system first " +
+                "and add courier marker to the map!");
+        } else if (deliveryMarkers[0] === undefined) {
+            alert("Please, add order details with address to the system first " +
+                "and add delivery marker to the map!");
+        } else {
+            for (let i = 0; i < couriersMarkers.length; i++) {
+                L.Routing.control({
+                    waypoints: [
+                        L.latLng(couriersMarkers[i]._latlng),
+                        L.latLng(deliveryMarkers[i]._latlng)
+                    ],
+                    lineOptions: {
+                        styles: [{color: 'green', opacity: 1, weight: 4}]
+                    },
+                    router: L.Routing.graphHopper('585def41-7ae7-4420-a2b3-b71c919bc166'),
+                    routeWhileDragging: true
+                }).addTo(myDeliveryServiceMap);
+            }
+        }
     });
-});
-
-
-/*
-L.Routing.control({
-    waypoints: [
-        L.latLng(55.684758, 37.738521),
-        L.latLng(55.826479, 37.487208),
-        L.latLng(55.694843, 37.435023),
-        L.latLng(55.790139, 37.814052)
-    ],
-    lineOptions: {
-        styles: [{color: 'blue', opacity: 1, weight: 4}]
-    },
-    router: L.Routing.graphHopper('585def41-7ae7-4420-a2b3-b71c919bc166'),
-    routeWhileDragging: true
-}).addTo(myDeliveryServiceMap);
-
-L.Routing.control({
-    waypoints: [
-        L.latLng(55.833436, 37.715175),
-        L.latLng(55.782392, 37.614924),
-        L.latLng(55.642063, 37.656123)
-    ],
-    lineOptions: {
-        styles: [{color: 'red', opacity: 1, weight: 4}]
-    },
-    router: L.Routing.graphHopper('585def41-7ae7-4420-a2b3-b71c919bc166'),
-    routeWhileDragging: true
-}).addTo(myDeliveryServiceMap);*/
+}));
--- src/main/resources/static/js/markers.js
+++ src/main/resources/static/js/markers.js
@@ -23,14 +23,20 @@ $(document).ready((function () {
             dataType: 'json',
             success: function (data) {
                 couriersMarkersLayerGroup.clearLayers();
-                for (let i = 0; i <= data.toString().length - 1; i++) {
-                    let latitude = data.result[i].latitude;
-                    let longitude = data.result[i].longitude;
-                    let courierMarker = L.marker([latitude, longitude],
-                        {icon: oldMan}).addTo(couriersMarkersLayerGroup);
-                    courierMarker.bindPopup("Курьер №" + [i + 1] + "<br>" +
-                        data.result[i].firstName + " " + data.result[i].lastName);
-                    couriersMarkers[i] = courierMarker;
+                if (data.result[0] === undefined) {
+                    alert("Please, add courier with coordinates to the system first!");
+                } else {
+                    for (let i = 0; i <= data.toString().length - 1; i++) {
+                        if (data.result[i] !== undefined) {
+                            let latitude = data.result[i].latitude;
+                            let longitude = data.result[i].longitude;
+                            let courierMarker = L.marker([latitude, longitude],
+                                {icon: oldMan}).addTo(couriersMarkersLayerGroup);
+                            courierMarker.bindPopup("Курьер №" + [i + 1] + "<br>" +
+                                data.result[i].firstName + " " + data.result[i].lastName);
+                            couriersMarkers[i] = courierMarker;
+                        }
+                    }
                 }
             }
         })
@@ -48,17 +54,23 @@ $(document).ready((function () {
             dataType: 'json',
             success: function (data) {
                 deliveryMarkersLayerGroup.clearLayers();
-                for (let i = 0; i <= data.toString().length - 1; i++) {
-                    let address = data.result[i].orderAddress;
-                    L.esri.Geocoding.geocode()
-                        .text(address)
-                        .run((err, results) => {
-                            let latitude = results.results[0].latlng.lat;
-                            let longitude = results.results[0].latlng.lng;
-                            let deliveryMarker = L.marker([latitude, longitude]).addTo(deliveryMarkersLayerGroup);
-                            deliveryMarker.bindPopup(address);
-                            deliveryMarkers[i] = deliveryMarker;
-                        });
+                if (data.result[0] === undefined) {
+                    alert("Please, add order details with delivery address to the system first!");
+                } else {
+                    for (let i = 0; i <= data.toString().length - 1; i++) {
+                        if (data.result[i] !== undefined) {
+                            let address = data.result[i].orderAddress;
+                            L.esri.Geocoding.geocode()
+                                .text(address)
+                                .run((err, results) => {
+                                    let latitude = results.results[0].latlng.lat;
+                                    let longitude = results.results[0].latlng.lng;
+                                    let deliveryMarker = L.marker([latitude, longitude]).addTo(deliveryMarkersLayerGroup);
+                                    deliveryMarker.bindPopup(address);
+                                    deliveryMarkers[i] = deliveryMarker;
+                                });
+                        }
+                    }
                 }
             }
         })

1671bdc887067be61be697d57a7d8b5065af8807
Mikhail Cheskiy <mihail.cheskij@gmail.com>
2019-09-13 15:24:01 +0300

    Исправил совместимость locations с текущим оформлением страниц

--- src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/controllers/LocationsController.java
+++ src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/controllers/LocationsController.java
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.entities.LocationsEntity;
+import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.tableEntities.LocationsEntity;
 import ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.repositories.LocationsRepository;
 
 

227d70acbe5ca1f6c2a6182886144b804f5bd92d
Artem Lebedev <Aurrum64@gmail.com>
2019-09-20 14:56:53 +0300

    Мелкий правки

--- src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/service/MailSender.java
+++ src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/service/MailSender.java
@@ -1,7 +1,6 @@
 package ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -15,9 +14,6 @@ public class MailSender {
     public MailSender(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
-
-    /*@Value("${spring.mail.username}")
-    private String username;*/
 
     public void send(String emailTo, String subject, String message) {
 

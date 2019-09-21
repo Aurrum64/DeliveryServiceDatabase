a1f3bb56dc1e5ed179b06c270b03e25c8d3fb374
Artem Lebedev <Aurrum64@gmail.com>
2019-09-22 00:11:08 +0300

    Travis check 3

--- src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/Application.java
+++ src/main/java/ru/ncedu/lebedev/deliveryService/deliveryServiceDatabase/Application.java
@@ -1,13 +1,13 @@
 package ru.ncedu.lebedev.deliveryService.deliveryServiceDatabase;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootApplication
-@TestPropertySource(value = "templates/application.properties")
-@PropertySource(value = "templates/application.properties")
+@PropertySource(value = "templates/application.properties", ignoreResourceNotFound = true)
 public class Application {
 
     public static void main(String[] args) {
